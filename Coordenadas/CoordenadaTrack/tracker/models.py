4d6ae3265abfe35edbd7ce1bfa2c5d414abf841f
Erick <erick.calderon@educa.madri.com>
2022-04-05 09:18:16 +0200

    modificaciones

--- Coordenadas/CoordenadaTrack/tracker/models.py
+++ Coordenadas/CoordenadaTrack/tracker/models.py
@@ -2,15 +2,6 @@ from django.db import models
 
 
 class Direccion(models.Model):
-    CALLE = 'c/'
-    AVENIDA = 'avd'
-    PLAZA = 'plza'
-
-    VIA_CHOISES=[
-        (CALLE, 'Calle'),
-        (AVENIDA, 'Avenida'),
-        (PLAZA, 'Plaza'),
-    ]
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
     longitud = models.DecimalField(max_digits=12, decimal_places=6)
