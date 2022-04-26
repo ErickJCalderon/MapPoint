174900d96955590f14cecc5122c6af659d3fcfcd
rodrigo <rodrigo.escribano77@gmail.com>
2022-04-26 12:17:21 +0200

    modficiaciones admin y models

--- Coordenadas/CoordenadaTrack/tracker/admin.py
+++ Coordenadas/CoordenadaTrack/tracker/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Direccion
+from .models import Direccion, Usuario
 
 """Registro del modelo Direccion"""
 admin.site.register(Direccion)
+admin.site.register(Usuario)
--- Coordenadas/CoordenadaTrack/tracker/models.py
+++ Coordenadas/CoordenadaTrack/tracker/models.py
@@ -1,4 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+"""Creaciond del modelo Usuarios"""
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    dni = models.CharField(max_length=9, unique=True)
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+
+    """Asignacion de FK al auth_user"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=200, unique=True)
+
+    class Meta:
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+
+    def __str__(self):
+        return self.nombre
+
 
 """Creacion del modelo Direccion"""
 class Direccion(models.Model):
@@ -7,9 +27,12 @@ class Direccion(models.Model):
     longitud = models.DecimalField(max_digits=12, decimal_places=6)
     latitud = models.DecimalField(max_digits=12, decimal_places=6)
 
+    """Asignacion de FK al modelo Usuario"""
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
     class Meta:
         verbose_name = "Coordenadas"
         verbose_name_plural = "Coordenadass"
 
     def __str__(self):
-        return self.id
+        return self.nombre
