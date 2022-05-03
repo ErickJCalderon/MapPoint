bec5124a810ebd593ecb5bc71ec1d4ff1291f6bb
rodrigo <rodrigo.escribano77@gmail.com>
2022-05-03 12:44:40 +0200

    documentacion

--- Coordenadas/CoordenadaTrack/tracker/api/serializers.py
+++ Coordenadas/CoordenadaTrack/tracker/api/serializers.py
@@ -5,21 +5,27 @@ from rest_framework import serializers
 from tracker.models import Direccion, Usuario
 
 """Serializamos el modelo de Usarios estandar"""
+
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
 
+
 """Serializamos el modelo de Direcciones que hemos creado"""
+
+
 class DireccionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Direccion
-        fields = ['id', 'nombre', 'longitud' , 'latitud', 'usuario']
+        fields = ['id', 'nombre', 'longitud', 'latitud', 'usuario']
+
 
 """Serializamos el modelo de Direcciones que hemos creado"""
+
+
 class Usuarioserializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
         fields = ['id', 'dni', 'nombre', 'apellido', 'user', 'email']
-
-
