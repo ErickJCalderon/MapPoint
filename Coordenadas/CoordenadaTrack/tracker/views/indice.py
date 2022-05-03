ac2a4a700f88b2b89d24399dfed75aebed4b88f4
rodrigo <rodrigo.escribano77@gmail.com>
2022-05-03 12:38:50 +0200

    documentacion

--- Coordenadas/CoordenadaTrack/tracker/views/indice.py
+++ Coordenadas/CoordenadaTrack/tracker/views/indice.py
@@ -11,26 +11,37 @@ def jsonView(request):
     if request.method == 'GET':
         datos = {}
 
+        """Hemos creado un diccionario datos y asignamos la funcion listar_puntos a lista para saber el estado 
+        que devuelve"""
         try:
             lista = listar_puntos()
+            """En caso de error, asignamos al diccionario dicho error para poder retornarlo mas adelante,
+             en caso de acierto se devuele la lista de todo lo que contenga en formato JsonResponse"""
             if 'error' in lista:
-                datos['error']= lista['error']
+                datos['error'] = lista['error']
             else:
                 datos['ok'] = lista['ok']
         except:
             datos['error'] = "Se ha producido un error en la respuesta del servidor"
         return JsonResponse(datos)
 
-"""Request para el registro de una direccion"""
+
+"""Variables para una prueba"""
 id_direccion = 1
 nombre_usuario = "Erick"
+
+"""Request para el registro de una direccion"""
 
 
 def direccion(request):
     if request.method == 'POST':
         datos = {}
+        """Hemos creado un diccionario datos y asignamos la funcion registrar_direcco con las variables de prueba
+           a direccion para saber el estado que devuelve"""
         try:
             direccion = registrar_direccion(id_direccion, nombre_usuario)
+            """En caso de error, asignamos al diccionario dicho error para poder retornarlo mas adelante,
+            en caso de acierto se devuele la lista de todo lo que contenga en formato JsonResponse"""
             if 'error' in direccion:
                 datos['error'] = direccion['error']
             else:
@@ -41,18 +52,5 @@ def direccion(request):
 
 
 """Request para la creacion de un usuario"""
-
-
-def usuario(request):
-    if request.method == 'POST':
-        usuario = crear_usuario()
-        messages.success("El usuario se registro exitosamente")
-        return JsonResponse(usuario)
-    else:
-        messages.error("No ha sido posible crearlo")
-
-
-
-
 
 
